f59ae4bc109ebfbd68ca47344aa8a415354f35cc
Higor3D <higorgomes07@gmail.com>
2022-08-17 20:18:27 -0300

    primeiro commit

---
+++ 1708.php
@@ -0,0 +1,24 @@
+<?php 
+
+	$data = "20 de Agosto de 2020";
+	$salario = 850.00;
+	$cargo = "Estagiario";
+	$idade = 18;
+	$resultado = true;
+
+	if ($resultado) {
+		echo "Verdadeiro";
+	}
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title>Aula PHP 17/08</title>
+ </head>
+ <body>
+ 
+ </body>
+ </html>
